a72066148aa2660b68e398597d61a6777dacfc94
Oleh <osukhoruchko@gmail.com>
2020-01-14 00:37:00 +0200

    Initial commit. Working

--- main.rb
+++ main.rb
@@ -1,3 +1,16 @@
+=begin
+puts "1 [+] add word to your vacabulary.\n2 [+] show vacabulary.\n3 [+] learning mode.\n4 [+] statistic.\n5 [+] exit."
+print "add first word yo your vacabulary. --> "
+puts "STATISTIC"
+puts "YOUR VACABULARY"
+puts "LEARNING MODE"
+puts "NEW WORDS"
+puts "LEARNED WORDS"
+puts "word --> "
+puts "translate --> "
+puts "Are you sure?\n1 [+] yes\n2 [+] no"
+=end
+
 current_path = File.dirname(__FILE__)
 
 require 'yaml'
@@ -6,10 +19,12 @@ require './vacabulary.rb'
 
 TOKEN = '911434810:AAGwi0QAMP3vV_asGIV6YuKdtpjvhwvZW64'
 
-vacabulary = Vacabulary.new(current_path)
+file_path = current_path + '/users.yml'
 
-Telegram::Bot::Client.run(TOKEN) do |bot|
-  bot.listen do |message|
+Telegram::Bot::Client.run(TOKEN) do |vacabularyBot|
+  vacabularyBot.listen do |mainMessage|
+
+    vacabulary = Vacabulary.new(file_path, vacabularyBot, mainMessage)
 
 =begin
     unless File.exist?(current_path + "/data/" + message.chat.id.to_s + "_new_words.txt")
@@ -17,8 +32,26 @@ Telegram::Bot::Client.run(TOKEN) do |bot|
     end
 =end
 
-    case message.text
+    case mainMessage.text
     when '/start', '/start start'
+
+      yamlFile = YAML.load(File.read(file_path))
+
+      unless yamlFile.key?(mainMessage.chat.id)
+        yamlFile[mainMessage.chat.id] = {:new_words => {}, :learned_words => {}}
+        File.write(file_path, yamlFile.to_yaml)
+        vacabularyBot.api.send_message(
+          chat_id: mainMessage.chat.id,
+          text: "Вітання!\nЯ твій персональний бот який поможе в вивчені англійських слів.\n/help для отримання всіх команд."
+        )
+      else
+        vacabularyBot.api.send_message(
+          chat_id: mainMessage.chat.id,
+          text: "З поверненням!\nДавай вивчимо ще декілька невідомих слів.\n/help для отримання всіх команд."
+        )
+      end
+
+=begin
       if File.exist?(current_path + "/data/" + message.chat.id.to_s + "_new_words.txt")
         bot.api.send_message(
           chat_id: message.chat.id,
@@ -31,26 +64,28 @@ Telegram::Bot::Client.run(TOKEN) do |bot|
           text: "Вітання!\nЯ твій персональний бот який поможе в вивчені англійських слів.\n/help для отримання всіх команд."
         )
       end
+=end
+
     when '/help'
-      bot.api.send_message(
-        chat_id: message.chat.id,
+      vacabularyBot.api.send_message(
+        chat_id: mainMessage.chat.id,
         text: "/add - adding new word.\n/show - your vacabulary.\n/learn - learning mode.\n/stat - show current statistic.\n\n/notification - learning by notification [on|off]."
       )
     when '/add'
-      vacabulary.add_word(bot, message)
+      vacabulary.add_word
     when '/show'
-
+      vacabulary.show_vacabulary
     when '/learn'
-
+      vacabulary.learning_mode
     when '/stat'
-
+      vacabulary.show_stat
     when '/notification'
 
-    when '/aborts'
+    when '/abortt'
       abort
     else
-      bot.api.send_message(
-        chat_id: message.chat.id,
+      vacabularyBot.api.send_message(
+        chat_id: mainMessage.chat.id,
         text: "I don't understand you...\nUse /help to see all command."
       )
     end
--- vacabulary.rb
+++ vacabulary.rb
@@ -1,61 +1,258 @@
 class Vacabulary
+  attr_reader :file_path, :bot, :message
 
-  def initialize(current_path)
-    @current_path = current_path
+  def initialize(file_path, bot, message)
+    @file_path = file_path
+    @bot = bot
+    @message = message
   end
 
-  def add_word(bot, message)
+  def add_word
     word = nil
     translate = nil
 
-    bot.api.send_message(
-      chat_id: message.chat.id,
+    @bot.api.send_message(
+      chat_id: @message.chat.id,
       text: "Word:"
     )
 
-    bot.listen do |message|
-      word = message.text
+    @bot.listen do |message|
+      word = message.text.capitalize
       break
     end
 
-    bot.api.send_message(
-      chat_id: message.chat.id,
+    return 0 if word == "/cancel"
+
+    @bot.api.send_message(
+      chat_id: @message.chat.id,
       text: "Translate:"
     )
 
-    bot.listen do |message|
-      translate = message.text
+    @bot.listen do |message|
+      translate = message.text.capitalize
       break
     end
 
-    bot.api.send_message(
-      chat_id: message.chat.id,
+    return 0 if translate == "/cancel"
+
+    yamlFile = YAML.load(File.read(@file_path))
+    puts yamlFile
+    yamlFile[@message.chat.id][:new_words][word] = [translate, 0]
+    File.write(@file_path, yamlFile.to_yaml)
+
+    @bot.api.send_message(
+      chat_id: @message.chat.id,
       text: "New word was saved."
     )
-
-    file = File.open(current_path + "/data/" + message.chat.id.to_s + "_new_words.txt", "a")
-    file.puts("#{word} #{translate}")
-    file.close
   end
 
   def show_vacabulary
+    yamlFile = YAML.load(File.read(@file_path))
 
+    if yamlFile[@message.chat.id][:new_words].size == 0
+      vacabulary = "You have not any word in your vacabulary.\n"
+    else
+      vacabulary = "New words\n- - - - - - - - - -\n"
+
+      yamlFile[@message.chat.id][:new_words].each do |word, translate|
+         vacabulary += word.ljust(20 - word.length) + translate[0] + "\n"
+      end
+    end
+
+    if yamlFile[@message.chat.id][:learned_words].size == 0
+      vacabulary += "\nYou dont learned any word."
+    else
+      vacabulary += "\nLearned words\n- - - - - - - - - - - - -\n"
+
+      yamlFile[@message.chat.id][:learned_words].each do |word, translate|
+        vacabulary += word.ljust(20) + translate[0] + "\n"
+      end
+    end
+
+    @bot.api.send_message(
+      chat_id: @message.chat.id,
+      text: vacabulary
+    )
+
+    puts vacabulary
   end
 
   def learning_mode
+    yamlFile = YAML.load(File.read(@file_path))
+    puts yamlFile.to_s
 
+    wordsFromFile = yamlFile[@message.chat.id][:new_words].values
+    puts wordsFromFile.to_s
+
+    userWord = nil
+    correct = 0
+    wrong = 0
+
+    if wordsFromFile.size == 0
+      @bot.api.send_message(
+        chat_id: @message.chat.id,
+        text: "I can't find any word in your vacabulary.\nYou can add new word /add"
+      )
+    else
+      @bot.api.send_message(
+        chat_id: @message.chat.id,
+        text: "Learning mode [ON]"
+      )
+
+      while true
+
+        break if wordsFromFile.size == 0
+
+        translatedWord = wordsFromFile.sample
+        puts translatedWord.to_s
+        puts wordsFromFile.index(translatedWord)
+
+        correctWord = yamlFile[@message.chat.id][:new_words].key(translatedWord)
+        puts correctWord.to_s
+
+        @bot.api.send_message(
+          chat_id: @message.chat.id,
+          text: translatedWord[0]
+        )
+
+        @bot.listen do |message|
+          userWord = message.text
+          break
+        end
+
+        break if userWord == "/end"
+
+        if userWord.capitalize == correctWord
+          @bot.api.send_message(
+            chat_id: @message.chat.id,
+            text: "Right!"
+          )
+
+          yamlFile[@message.chat.id][:new_words][correctWord][1] += 1
+
+          correct += 1
+
+          if yamlFile[@message.chat.id][:new_words][correctWord][1] == 5
+            yamlFile[@message.chat.id][:learned_words][correctWord] = translatedWord
+            yamlFile[@message.chat.id][:new_words].delete(correctWord)
+          end
+
+        else
+          @bot.api.send_message(
+            chat_id: @message.chat.id,
+            text: "Wrong!\nCorrect word: #{correctWord}"
+          )
+
+          wrong += 1
+        end
+
+        wordsFromFile.delete_at(wordsFromFile.index(translatedWord)).to_s
+
+      end
+
+      File.write(@file_path, yamlFile.to_yaml)
+
+
+      @bot.api.send_message(
+        chat_id: @message.chat.id,
+        text: "Correct: #{correct}\nWrong: #{wrong}"
+      )
+
+      @bot.api.send_message(
+        chat_id: @message.chat.id,
+        text: "Learning mode [OFF]"
+      )
+
+    end
+
+=begin
+    yamlFile = YAML.load(File.read(@file_path))
+    fileWords = yamlFile[@message.chat.id][:new_words].values
+    puts fileWords.size
+    userWord = ""
+    correct = 0
+    wrong = 0
+
+    if fileWords.size == 0
+      @bot.api.send_message(
+        chat_id: @message.chat.id,
+        text: "I can't find any word in your vacabulary.\nYou can add new word /add"
+      )
+    else
+      @bot.api.send_message(
+        chat_id: @message.chat.id,
+        text: "Learning mode [ON]"
+      )
+
+      while userWord != "/end" || fileWords.size != 0
+
+        translate = fileWords.sample
+
+        puts translate.to_s
+
+        correctWord = yamlFile[@message.chat.id][:new_words].key(translate)
+        puts correctWord
+
+        @bot.api.send_message(
+          chat_id: @message.chat.id,
+          text: translate[0]
+        )
+
+        @bot.listen do |message|
+          userWord = message.text
+          break
+        end
+
+        if userWord == correctWord
+
+          @bot.api.send_message(
+            chat_id: @message.chat.id,
+            text: "Right!"
+          )
+
+          yamlFile[@message.chat.id][:new_words][correctWord[1]] = translate[1] + 1
+          correct += 1
+        else
+
+          @bot.api.send_message(
+            chat_id: @message.chat.id,
+            text: "Wrong!\nCorrect word: #{correctWord}"
+          )
+
+          wrong += 1
+        end
+
+
+        puts fileWords
+        puts fileWords.shift(fileWords.index(translate))
+        puts userWord
+        puts fileWords
+
+      end
+
+      File.write(@file_path, yamlFile.to_yaml)
+
+      @bot.api.send_message(
+        chat_id: @message.chat.id,
+        text: "Learning mode [OFF]"
+      )
+    end
+=end
   end
 
   def show_stat
+    yamlFile = YAML.load(File.read(@file_path))
 
+    new_words = yamlFile[@message.chat.id][:new_words].size
+    learned_words = yamlFile[@message.chat.id][:learned_words].size
+
+    @bot.api.send_message(
+      chat_id: @message.chat.id,
+      text: "STATISTIC\nNew words: #{new_words}\nLearned words: #{learned_words}"
+    )
   end
 
   def notification
 
   end
-
-  def current_path
-    @current_path
-  end
-
 end
